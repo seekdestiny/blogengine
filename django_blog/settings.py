2cc94a9739ce569e4f789890ab2eacb2a00f1f30
seekdestiny <jifeiqian@gmail.com>
2018-03-04 17:29:35 -0800

    try heroku again

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!5^iasxx13a4nu4pz0ym!oq#h+=k!u*zcdlc-e@5c996!5q8&('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -161,8 +161,8 @@ PROJECT_APPS = ['blogengine']
 
 # Heroku config
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config(default="sqlite:///db.sqlite3")
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config(default="sqlite:///db.sqlite3")
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
