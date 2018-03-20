41f72cc78d3b4eae4c30a40036768590efa68dbc
seekdestiny <jifeiqian@gmail.com>
2018-03-19 22:06:27 -0700

    set heroku fab deploy again

--- fabfile.py
+++ fabfile.py
@@ -7,9 +7,12 @@ def deploy():
     """
     # Push changes to Bitbucket
     local("git push origin master")
-
+    
     # Push changes to Heroku
     local("git push heroku master")
 
     # Run migrations on Heroku
     local("heroku run python manage.py migrate")
+
+    # Push changes to AWS
+    #local("eb deploy")
